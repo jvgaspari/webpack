34975c78f29b4c047afbb84a156d94ddd5a6cba7
José Victor Gaspari <josevictorgaspari@gmail.com>
2022-08-29 20:10:19 -0300

    feat: add html-webpack-plugin

--- webpack.config.js
+++ webpack.config.js
@@ -2,12 +2,14 @@ const path = require('path');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const TerserPlugin = require('terser-webpack-plugin');
 const webpack = require('webpack');
-const dotenvPlugin = require('dotenv-webpack');
+const DotenvPlugin = require('dotenv-webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
     entry: './src/index.js',
     output: {
-        filename: "bundle.js",
+        filename: "[name].[contenthash].js",
         path: path.resolve(__dirname, 'dist')
     },
     mode: 'development',
@@ -61,13 +63,15 @@ module.exports = {
         ]
     },
     plugins: [
+        new CleanWebpackPlugin(),
         new MiniCssExtractPlugin({
-            filename: 'styles.css'
+            filename: '[name].[contenthash].css'
         }),
         new webpack.DefinePlugin({
             VERSION: JSON.stringify('1.0.1'),
             PORT: JSON.stringify('8080')
         }),
-        new dotenvPlugin(),
+        new DotenvPlugin(),
+        new HtmlWebpackPlugin(),
     ]
 }
